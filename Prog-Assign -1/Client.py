710cd95a3f29da73d003100e640abb3b75cf6544
jun <junyi@chapman.edu>
2025-09-19 09:18:32 -0700

    Added folder for first assignment
    Inside there is the initial .py's

---
+++ Prog-Assign -1/Client.py
@@ -0,0 +1,22 @@
+#import socket module
+import socket
+import sys # In order to terminate the program
+
+FILENAME = "helloworld.html"
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    server = socket.gethostbyname("localhost")
+except socket.gaierror:
+    print("Failed to resolve host name")
+    sys.exit()
+
+clientSocket.connect((server,80))
+request = "GET /"+FILENAME+" HTTP/1.1\r\nHost: localhost\r\n\r\n"
+clientSocket.send(request.encode())
+responce = clientSocket.recv(1024).decode()
+
+print(responce)
+
+clientSocket.close()
