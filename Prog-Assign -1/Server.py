c00b4cb2fc8495ad238806f9793859a76f5eafba
Jun Yi <yijunho117@gmail.com>
2025-09-21 11:34:58 -0700

    updated client and server
    added helloworld.html, changed client+server.py port number to 6789 from 80

--- Prog-Assign -1/Server.py
+++ Prog-Assign -1/Server.py
@@ -3,7 +3,7 @@ from socket import *
 import sys # In order to terminate the program
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
-serverSocket.bind(('localhost',80))
+serverSocket.bind(('localhost', 6789))
 serverSocket.listen(5)
 
 while True:
